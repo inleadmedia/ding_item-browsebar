463e6bbb82660ad9cbdc75ca4dcf233d70b92d9d
Martin Cording <mc@inleadmedia.com>
2011-07-11 18:17:24 +0200

    Customized the breadcrumb.

--- template.php
+++ template.php
@@ -1,19 +1,29 @@
 <?php
 
+
 /* Put Breadcrumbs in a ul li structure */
 function easyting_breadcrumb($variables) {
   $breadcrumb = $variables['breadcrumb'];
-
   if (!empty($breadcrumb)) {
-      $crumbs = '<ul class="breadcrumbs">';
-
-      foreach($breadcrumb as $value) {
-           $crumbs .= '<li>'.$value.'</li>';
+    $crumbs = '<ul class="breadcrumbs">';
+    $array_size = count($breadcrumb);
+    $i = 0;
+    while ( $i < $array_size) {
+      $crumbs .= '<li class="';
+      if ($i == 0) {
+	$crumbs .= ' home';
       }
-      $crumbs .= '</ul>';
+      if ($i+1 == $array_size) {
+	$crumbs .= ' active';
+      }
+      $crumbs .=  '">' . $breadcrumb[$i] . '</li>';
+      $i++;
     }
-      return $crumbs;
+    $crumbs .= '</ul>';
+    return $crumbs;
   }
+}
+
 
 
 ?>
