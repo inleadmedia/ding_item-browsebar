ae715ee0856e7c2df8b01de18b496bb18d916b6a
Martin Cording <mc@inleadmedia.com>
2011-07-12 16:41:21 +0200

    Added menus as blocks in the navigation region and added js for dropdown effect.

---
+++ js/dropdown.js
@@ -0,0 +1,12 @@
+(function ($) {
+  $(document).ready(function() {
+    $('#block-system-main-menu li').hover(
+      function(){
+        $('ul:first', $(this)).show();
+      },
+      function(){
+        $('ul', $(this)).hide();
+      }
+    );
+  });
+})(jQuery);
