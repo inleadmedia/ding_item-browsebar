ee82d70bcb9c9fa18bc414bf99f7e1b72183caa3
AnatolyMuntean <am@inleadmedia.com>
2011-08-01 16:54:22 -0400

    case 5713 - Added primary menu namiation.

---
+++ js/easyting.js
@@ -0,0 +1,14 @@
+(function ($) {
+  $(document).ready(function() {
+    $('#primary-nav ul li').mouseover(function() {
+      $(this).addClass('active');
+      $(this).find('img').css({'opacity':'0'});
+    });
+
+    $('#primary-nav ul li').mouseout(function() {
+      $(this).parent().find('li').removeClass('active');
+      $(this).removeClass('active');
+      $(this).find('img').css({'opacity':'1'});
+    });
+  });
+})(jQuery);
