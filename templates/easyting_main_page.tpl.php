2ab543bdfeb71404f28d08aa86b13e236af3b958
Martin Cording <mc@inleadmedia.com>
2011-08-09 14:05:24 +0200

    case 5713 - Review: Missing space.

--- templates/easyting_main_page.tpl.php
+++ templates/easyting_main_page.tpl.php
@@ -311,7 +311,7 @@
       </div>
       <div class="clear"></div>
       <div class="on-map">
-        <img class="left"src="/<?php print path_to_theme(); ?>/images/map.png" width="50" height="50" alt="" />
+        <img class="left" src="/<?php print path_to_theme(); ?>/images/map.png" width="50" height="50" alt="" />
         <p class="map-link left"><a href="#"><?php print t('Se på kort'); ?></a></p>
         <div class="clear"></div>
       </div>
