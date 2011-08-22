8c99f9c49c132dd99b04a0b3ced515681c457bdc
Eugene Chorba <ec@inlead.dk>
2011-08-22 15:58:18 +0300

    case 11961 - Add js functionality popups to work properly.

--- js/dropdown.js
+++ js/dropdown.js
@@ -8,5 +8,29 @@
         $('ul', $(this)).hide();
       }
     );
+
+    // Header navigation subnav popup
+    $('.subnav-trigger').click(function(e){
+      e.preventDefault();
+      e.stopPropagation();
+
+      var popupId = $(this).attr('href');
+      var popup = $(popupId);
+      if (popup.is(':visible')) {
+        // Hide all popups
+        $(document).click();
+      }
+      else {
+        // Hide all popups
+        $(document).click();
+        // Show popup
+        $(popupId).show();
+      }
+    });
+    // Hide all subnav popups on all document clicks
+    $(document).click(function(){
+      $('.subnav-popup').hide();
+    });
+
   });
 })(jQuery);
