b0ffc519ab89142e5f387336337409ae6abd392a
AnatolyMuntean <am@inlead.dk>
2011-12-12 15:36:14 +0200

    case 13197 - Alt attribute for IE.

--- templates/carousel_items.tpl.php
+++ templates/carousel_items.tpl.php
@@ -50,7 +50,7 @@
       $title = (drupal_strlen($item->title) > 18) ? drupal_substr($item->title, 0, 18) . '...' : $item->title;
     ?>
 
-      <h1><?php print l($title,'ting/object/' . $item->id, array('attributes' => array('title' => $item->title))) ?></h1>
+      <h1><?php print l($title,'ting/object/' . $item->id, array('attributes' => array('title' => $item->title, 'alt' => $item->title))) ?></h1>
       <p>
         <?php if (isset($item->creator)): ?>
           <?php print t('By') ?> <span class="creator"><?php print $item->creator ?></span> <?php print $item->year ? "({$item->year})" : '' ?>
